7a2353d9e1fd706ed0c918592890d35b0ce131b3
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-04-13 20:39:39 +0200

    Stats & JavaScript Increment

--- js/script.js
+++ js/script.js
@@ -13,4 +13,30 @@ function userScroll() { // To have the navbar a dark background when scroll on t
     });
 }
   
+function incrementStats() {
+  const counters = document.querySelectorAll('.counter'); // To get all counter elements into an array like node list
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;  // To initially clear the state
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');  // + to covert the string into a number
+      const c = +counter.innerText;
+
+      const increment = target / 200; // Increment steps
+
+      if (c < target) { // Counts up as long c below the target
+        counter.innerText = Math.ceil(c + increment); // To have the inner text the sum of the c value and the increment (ceil rounds the resulting number up)
+        setTimeout(updateCounter, 1); // To run the function recusivelly and wait 1 ms
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+// Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
