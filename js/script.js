0ae7df3ff1a9c431aa9bb7b9af714155275c623b
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-04-14 18:09:52 +0200

    Projects & Testimonials

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 // Add custom JavaScript here
 function userScroll() { // To have the navbar a dark background when scroll on the y axis is > 50px
     const navbar = document.querySelector('.navbar');
+    const toTopBtn = document.querySelector('#to-top');
   
     window.addEventListener('scroll', () => {
       if (window.scrollY > 50) {
-        navbar.classList.add('bg-dark');  // To add the dark bg
-        navbar.classList.add('navbar-sticky');  // To have the dark bg a bit transparent and have a shaddow - see in styles.scss
+        navbar.classList.add('navbar-sticky');  // To have a light bg which is a bit transparent and have a shaddow - see in styles.scss
       } else {  // On the top of the page (without scrolling)
-        navbar.classList.remove('bg-dark');
         navbar.classList.remove('navbar-sticky');
       }
     });
